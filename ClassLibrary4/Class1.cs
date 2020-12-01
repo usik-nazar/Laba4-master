5df4efbb93d7929165900de200e5f6e675416505
usik-nazar <nazar.usick200@gmail.com>
2020-12-01 17:01:00 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary4/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryLab04
+{
+    public class Class1
+    {
+        public static double[] CreateArrayFromTemplate(double startX, double dx, int size, Func<double, double> func)
+        {
+            double[] data = new double[size];
+            double x = startX;
+            for (int i = 0; i < size; ++i)
+            {
+                data[i] = func(x);
+                x += dx;
+            }
+            return data;
+        }
+
+        public static void BubbleSort(double[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = i + 1; j < data.Length; j++)
+                {
+                    if (data[i] < data[j])
+                    {
+                        double t = data[i];
+                        data[i] = data[j];
+                        data[j] = t;
+                    }
+                }
+
+            }
+        }
+        public static void PrintArray(double[] data)
+        {
+            for (int i = data.GetLowerBound(0); i <= data.GetUpperBound(0); ++i)
+            {
+                Console.WriteLine("arr[{0}]={1:0.0000}", i, data[i]);
+            }
+        }
+
+        public static void ProcessArray(double[] data, out double aMin, out double aMax, out double aAvg)
+        {
+            double S = data[0];
+            aMin = aMax = S;
+            for (int i = data.GetLowerBound(0) + 1; i <= data.GetUpperBound(0); ++i)
+            {
+                S += data[i];
+
+                if (aMin > data[i])
+                {
+                    aMin = data[i];
+                }
+
+                if (aMax < data[i])
+                {
+                    aMax = data[i];
+                }
+            }
+
+            aAvg = S / data.Length;
+        }
+
+        public static int PostProcessArray(double[] data, Predicate<double> pred)
+        {
+            int count = 0;
+            for (int i = data.GetLowerBound(0); i <= data.GetUpperBound(0); ++i)
+            {
+                if (pred(data[i]))
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Laba4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab04
+{
+    class Program
+    {
+        static double Func2(double x1, double x2)
+        {
+            double a,b ; 
+            a = 6 - Math.Cos(3 + x1);
+            b = 34 - 9 * Math.Pow(x2, 3) + x2;
+
+
+            return a / b  ;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            double startX = 1, dx = 0.5;
+            const int size = 500;
+            double[] arr = ClassLibraryLab04.Class1.CreateArrayFromTemplate(startX, dx, size, x => Func2(x * 2.76, x * 0.5));
+            ClassLibraryLab04.Class1.BubbleSort(arr);
+            ClassLibraryLab04.Class1.PrintArray(arr);
+
+            ClassLibraryLab04.Class1.ProcessArray(arr, out double min, out double max, out double avg);
+                      
+            Console.WriteLine("Мiнiмальне значення масиву: {0:0.0000}", min);
+            Console.WriteLine("Максимальне значення масиву: {0:0.0000}", max);
+            Console.WriteLine("Середнє значення масиву: {0:0.0000}", avg);
+          
+            int count = ClassLibraryLab04.Class1.PostProcessArray(arr, x => x >= 0.9 * avg && x <= 1.1 * avg);
+            Console.WriteLine("R={0}", count);
+            Console.WriteLine();
+
+        }
+    }
+}
